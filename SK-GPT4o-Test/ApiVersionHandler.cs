d63ce151b43884e0ae037155d6fe61d81f7dd8aa
Takashi Okawa <takashi.okawa@microsoft.com>
2024-05-22 05:13:24 +0900

    プロジェクト ファイルを追加します。

---
+++ SK-GPT4o-Test/ApiVersionHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SK_GPT4o_Test
+{
+    // based on the following document
+    // https://github.com/microsoft/semantic-kernel/issues/2833
+
+    public class ApiVersionHandler : DelegatingHandler
+    {
+        private const string ApiVersionKey = "api-version";
+        private const string NewApiVersion = "2024-04-01-preview";
+
+        public ApiVersionHandler() : base(new HttpClientHandler())
+        {
+
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            var uriBuilder = new UriBuilder(request.RequestUri!);
+            var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+
+            if (query[ApiVersionKey] == null) return await base.SendAsync(request, cancellationToken);
+
+            query[ApiVersionKey] = NewApiVersion;
+            uriBuilder.Query = query.ToString();
+            request.RequestUri = uriBuilder.Uri;
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ SK-GPT4o-Test/Program.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.Extensions.Configuration;
+
+namespace SK_GPT4o_Test
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            string? endpoint = configuration["Endpoint"];
+            string? deployName = configuration["DeployName"];
+            string? apiKey = configuration["ApiKey"];
+
+            if (endpoint is null || deployName is null || apiKey is null)
+            {
+                Console.WriteLine("Please set Azure OpenAI credentials in appsettings.json.");
+
+                return;
+            }
+
+            // Handle the API version manually
+            var client = new HttpClient(new ApiVersionHandler());
+
+            // Create kernel
+            var builder = Kernel.CreateBuilder();
+            builder.AddAzureOpenAIChatCompletion(deployName, endpoint, apiKey,httpClient:client);
+            var kernel = builder.Build();
+
+            // Create chat history
+            var history = new ChatHistory();
+
+            // Get chat completion service
+            var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+            // Start the conversation
+            Console.Write("User (press CTRL+Z to exit) > ");
+            string? userInput;
+            while (!String.IsNullOrEmpty(userInput = Console.ReadLine()))
+            {
+                // Add user input
+                history.AddUserMessage(userInput);
+
+                // Enable auto function calling
+                OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+                {
+                    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+                };
+
+                // Get the response from the AI
+                var result = await chatCompletionService.GetChatMessageContentAsync(
+                    history,
+                    executionSettings: openAIPromptExecutionSettings,
+                    kernel: kernel);
+
+                // Print the results
+                Console.WriteLine("Assistant > " + result);
+
+                // Add the message from the agent to the chat history
+                history.AddMessage(result.Role, result.Content ?? string.Empty);
+
+                // Get user input again
+                Console.Write("User (press CTRL+Z to exit) > ");
+            }
+        }
+    }
+}
